127816df85ceecd724026bbd594392df185082e5
MariaF01-art <maria.soares.barbosa01@escola.pr.gov.br>
2026-06-18 08:39:21 -0300

    yelloe.letter

--- script.js
+++ script.js
@@ -11,7 +11,15 @@ const botao = document.querySelector("button")
 const tamanho = document.getElementById("tamanho")
 tamanho.addEventListener("click", aumentarFonte)
 let fonte = 16;
+
 //função
 function aumentarFonte(){
+    fonte++;
     document.body.style.fontSize = fonte + "px";
 }
+
+//Botão contraste
+const Contraste =  document.getElementById("contraste") 
+contraste.addEventListener("click",mudar_o_contraste_para_amarelo )
+//função
+function mudar_o_contraste_para_amarelo(){ document.body.style.color = "yellow" }
